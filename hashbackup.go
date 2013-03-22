17676848970e585efcef1322c8f8123e3290e8d5
James Healy <james@yob.id.auto>
2013-03-23 12:47:36 +1300

    handle errors when walking the directory

--- hashbackup.go
+++ hashbackup.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"log"
 	"path/filepath"
 	"os"
 	"flag"
@@ -9,24 +10,26 @@ import (
 
 // returns a slice of file paths the user is interested in. root is the top
 // level directory to search under
-func getPathsOfInterest(root string) []string {
-	allPaths := []string{}
+func getPathsOfInterest(root string) (allPaths []string, err error) {
 	var scan = func(path string, _ os.FileInfo, inpErr error) (err error) {
 		fullpath, err := filepath.Abs(path)
 		if err != nil {
-			return nil
+			return
 		}
 		allPaths = append(allPaths, fullpath)
-		return nil
+		return
 	}
-	filepath.Walk(root, scan)
-	return allPaths
+	err = filepath.Walk(root, scan)
+	return
 }
 
 // it's alive!
 func main() {
 	flag.Parse()
-	allPaths := getPathsOfInterest(flag.Arg(0))
+	allPaths, err := getPathsOfInterest(flag.Arg(0))
+	if err != nil {
+		log.Fatal("Error walking directory")
+	}
 	for _, path := range allPaths {
 		fmt.Printf("Scanned %s\n", path)
 	}
