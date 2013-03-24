573828ecafd7e6b7ccbf361b731ee0a481986560
James Healy <james@yob.id.auto>
2013-03-24 14:56:55 +1300

    refactor some common test code into a helper

--- hashbackup_test.go
+++ hashbackup_test.go
@@ -8,19 +8,20 @@ import (
 	"testing"
 )
 
-func TestGenHashWithMd5(t *testing.T) {
+func sampleFile() string {
 	_, filename, _, _ := runtime.Caller(0)
-	f := path.Join(path.Dir(filename), "test_data","foo.txt")
-	result := genHash(md5.New(), f)
+	return path.Join(path.Dir(filename), "test_data","foo.txt")
+}
+
+func TestGenHashWithMd5(t *testing.T) {
+	result := genHash(md5.New(), sampleFile())
 	if result != "4221d002ceb5d3c9e9137e495ceaa647" {
 		t.Error("md5 not correct")
 	}
 }
 
 func TestGenHashWithSha1(t *testing.T) {
-	_, filename, _, _ := runtime.Caller(0)
-	f := path.Join(path.Dir(filename), "test_data","foo.txt")
-	result := genHash(sha1.New(), f)
+	result := genHash(sha1.New(), sampleFile())
 	if result != "804d716fc5844f1cc5516c8f0be7a480517fdea2" {
 		t.Error("sha1 not correct")
 	}
